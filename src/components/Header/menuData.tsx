76d1d1d552269759bc1fb2c397ec729443231576
shubhammanmohan <shubhammanmohan16@gmail.com>
2025-09-10 21:23:36 +0530

    first commit

---
+++ src/components/Header/menuData.tsx
@@ -0,0 +1,48 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "About",
+    path: "/about",
+    newTab: false,
+  },
+  {
+    id: 33,
+    title: "Blog",
+    path: "/blog",
+    newTab: false,
+  },
+  {
+    id: 3,
+    title: "Support",
+    path: "/contact",
+    newTab: false,
+  },
+  {
+    id: 4,
+    title: "Services",
+    newTab: false,
+    submenu: [
+      {
+        id: 41,
+        title: "Certification",
+        path: "/certification",
+        newTab: false,
+      },
+      {
+        id: 42,
+        title: "Internship",
+        path: "/internship",
+        newTab: false,
+      },
+    ],
+  },
+];
+export default menuData;
