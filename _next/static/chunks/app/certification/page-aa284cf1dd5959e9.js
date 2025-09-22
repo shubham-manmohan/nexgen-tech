cd64513b00935825f5a3ddc0a69452bffc578a32
shubhammanmohan <shubhammanmohan16@gmail.com>
2025-09-23 00:45:53 +0530

    web-commit

---
+++ _next/static/chunks/app/certification/page-aa284cf1dd5959e9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220,763],{3972:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.t.bind(n,1356,23))}},e=>{e.O(0,[619,356,441,255,358],()=>e(e.s=3972)),_N_E=e.O()}]);
